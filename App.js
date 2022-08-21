b781d78cbb3c0cf29eba1a10bb1228fac89619c9
kurnaztari <kurnaztari@ecs.vuw.ac.nz>
2022-08-21 18:07:10 +1200

    Added firebase to the app. It is not getting used yet.

--- App.js
+++ App.js
@@ -5,7 +5,28 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import AddTask from "./components/AddTask";
 import RemainingTasks from "./components/RemainingTasks";
 import TaskView from "./components/TaskView";
-import taskView from "./components/TaskView";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9372ilKlS0Rqz3QQkD7NtbSnGehe6THk",
+    authDomain: "swen325-assignment2-8f38c.firebaseapp.com",
+    projectId: "swen325-assignment2-8f38c",
+    storageBucket: "swen325-assignment2-8f38c.appspot.com",
+    messagingSenderId: "1071550186475",
+    appId: "1:1071550186475:web:6c8b75aeb7a52d0c56cfae",
+    measurementId: "G-FS707DZVZC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const Stack = createNativeStackNavigator();
 
