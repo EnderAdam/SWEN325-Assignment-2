6e72e877d9987ce7f026a1ae96cd4fc38d8e8f78
kurnaztari <kurnaztari@ecs.vuw.ac.nz>
2022-08-22 16:48:42 +1200

    Combined previously done task adding screens and login screens.

--- navigation/userStack.js
+++ navigation/userStack.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer, useNavigationContainerRef} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import MainScreen from '../screens/HomeScreen';
+import {Button, Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import RemainingTasks from "../screens/RemainingTasks";
+import AddTask from "../screens/AddTask";
+import TaskView from "../screens/TaskView";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import {getAuth} from "firebase/auth";
 
 const Stack = createStackNavigator();
+const auth = getAuth();
 
 export default function UserStack() {
+    const navigationRef = useNavigationContainerRef();
+
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={MainScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View style={{flex: 1}}>
+            <NavigationContainer ref={navigationRef}>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="RemainingTasks"
+                        component={RemainingTasks}
+                        initialParams={{task: ''}}
+                        options={{
+                            headerTitleAlign: 'center', title: 'Tasks',
+                            headerRight: () => (
+                                <TouchableOpacity onPress={() => navigationRef.navigate('AddTask')}>
+                                    <Image source={require('../Images/plusIcon.jpg')}
+                                           style={{width: 24, height: 24, marginRight: 16}}/>
+                                </TouchableOpacity>
+                            ),
+                            headerLeft: () => (
+                                <Button title="Sign Out" style={styles.button} onPress={() => auth.signOut()} />
+                            )
+                        }}
+                    />
+                    <Stack.Screen name="Profile" component={CompletedTasks}/>
+                    <Stack.Screen name='AddTask' component={AddTask}/>
+                    <Stack.Screen name='TaskView' component={TaskView} options={{title: TaskView.name}}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </View>
     );
 }
+
+const CompletedTasks = ({navigation, route}) => {
+    return (<View>
+            <Text>This is {route.params.name}'s profile</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    signOutButton: {
+        marginTop: 10
+    }
+});
