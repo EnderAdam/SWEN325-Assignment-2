8891cfb6409a6e97827069f21ee46843df2d4df5
kurnaztari <kurnaztari@ecs.vuw.ac.nz>
2022-08-28 18:25:15 +1200

    Added some formatting to TaskView screen.

--- screens/TaskView.js
+++ screens/TaskView.js
@@ -1,14 +1,24 @@
-import {Text, View} from "react-native";
-import {useEffect} from "react";
+import {Image, StyleSheet, Text, View} from "react-native";
+import React, {useEffect} from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 
 const TaskView = ({navigation, route}) => {
     useEffect(() => {
         navigation.setOptions({title: route.params.task.name});
     } , [route.params.task.name]);
-    return (<View>
-            <Text>This is {route.params.task.name}'s profile</Text>
-            <Text>{route.params.task.details}</Text>
+    return (
+        <View>
+            <View style={styles.iconLine}>
+                <Image source={require('../Images/infoIcon.jpg')}
+                       style={{width: 24, height: 24, marginRight: 10, marginLeft: 10}}/>
+                <Text style={styles.text}>Details: {route.params.task.details}</Text>
+            </View>
+            <View style={styles.iconLine}>
+                <Image source={require('../Images/calendarIcon.jpg')}
+                       style={{width: 24, height: 24, marginRight: 10, marginLeft: 10}}/>
+                <Text style={styles.text}>Details: {route.params.task.details}</Text>
+            </View>
             <Text>{route.params.task.plannedDate}</Text>
             <Text>{route.params.task.completedDate}</Text>
             <Text>{route.params.task.people}</Text>
@@ -20,3 +30,15 @@ const TaskView = ({navigation, route}) => {
 
 
 export default TaskView;
+
+const styles = StyleSheet.create({
+    iconLine: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+        marginTop: 16
+    },
+    text: {
+        fontSize: 16,
+    }
+});
